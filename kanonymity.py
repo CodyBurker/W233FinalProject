48ddaa859c20fede9686d5c3ec4a04f89d91dbaf
EC2 Default User <ec2-user@ip-172-31-58-69.ec2.internal>
2021-10-11 20:03:42 +0000

    Added comments

--- kanonymity.py
+++ kanonymity.py
@@ -1,43 +1,53 @@
-# Given a list of attributes and a table
-# List k-anonymity, and whether hidden attributes are still private (ranges, uniqueness based on class?)
-# This might be helpful: https://medevel.com/flask-tutorial-upload-csv-file-and-insert-rows-into-the-database/
-
-# Dataset: https://www.kaggle.com/parulpandey/2020-it-salary-survey-for-eu-region
+"""
+Module to test for k-anonymity in the dataset.
+"""
 
 import pandas as pd
 import pytest
 
+
 def read_example_dataset():
-# Read example dataset
+    """
+    Read example dataset
+    """
     example_dataframe = pd.read_csv('IT Salary Survey EU  2020.csv')
     return example_dataframe
+    
 
 def get_k_table(dataset, q_identifiers):
+    """
+    Given a dataset and list of QIDs, return a table of 
+    equivilence classes, with the number of records associated
+    with each class.
+    """
     k_table = dataset.groupby(q_identifiers).size().reset_index()
     k_table = k_table.rename({k_table.columns[-1]: 'KCount'}, axis=1)
     return k_table
 
-# Given a dataframe and list of QIDs, return the k-Anonymity of the table
+
 def get_k(dataset, q_identifiers):
+    """
+    Given a dataframe and list of QIDs, return the k-Anonymity of the table.
+    """
     k = min(get_k_table(dataset,q_identifiers)['KCount'])
     return k
 
-# Given a dataframe and a list of QIDs, return the equivilence classes with the the smallest K value
 def smallest_classes(dataset,q_identifiers):
+    """
+    Given a dataframe and a list of QIDs, return the equivilence class(es) with the the smallest k-value
+    """
     k_table = get_k_table(dataset, q_identifiers)
     min_equiv_classes = k_table[k_table['KCount'] == min(k_table['KCount'])]
-    # k = get_k(dataset, q_identifiers)
     return min_equiv_classes
 
 if __name__=="__main__":
-    
     dataset = pd.read_csv("HW3.csv")
-    # HW 3 1(c) k=31
+    # Get distinct k value for q-attributes = ['Sex']
     k = get_k(dataset, ['Sex'])
     print('K [Sex]:\t%i' % k)
-    # HW 3 1(d) k=10
+    # Get distinct k value for q-attributes = ['Sex','Age']
     k = get_k(dataset, ['Sex','Age'])
     print('K [Sex,Age]:\t%i' % k)
-    # HW 3 1(e) k=2
+    # Get distinct k value for q-attributes = ['Sex','Age','Birth Country']
     k = get_k(dataset, ['Sex','Age','Birth Country'])
     print('K [Sex,Age,Birth Country]:\t%i' % k)
--- ldiversity.py
+++ ldiversity.py
@@ -1,59 +1,64 @@
+"""
+Module to test for various l-diversity metrics within a dataset.
+"""
 import pandas as pd
 import numpy as np
 import pytest
 
+
 def read_example_dataset():
-# Read example dataset
+    '''
+    Read example dataset
+    '''
     example_dataframe = pd.read_csv('IT Salary Survey EU  2020.csv')
     return example_dataframe
 
-# Given a dataframe, quasi-identifiers, and a sensitive columns
-# Return a dataframe with each equivilence classes as a row and the count of distinct sensitive
-# Values for that class
+
 def get_l_distinct_table(dataset, q_identifiers, sensitive_column):
+    '''
+    Given a dataframe, quasi-identifiers, and a sensitive column
+    Return a dataframe with each equivilence classes as a row and the count of distinct sensitive
+    Values for that class
+    '''
     ldistinct = dataset.groupby(q_identifiers).agg({sensitive_column:"nunique"}).reset_index()
     # Rename Column
     ldistinct = ldistinct.rename({ldistinct.columns[-1]: 'LDistinct'}, axis=1)
     return ldistinct
 
+
 def get_l_distinct(dataset, q_identifiers, sensitive_column):
+    """
+    Given a dataframe, a list of quasi-identifiers, and a sentive column, return
+    the largest l value that the table is l-distinct for.
+    """
     l_distinct_table = get_l_distinct_table(dataset, q_identifiers, sensitive_column)
     return min(l_distinct_table['LDistinct'])
 
-# TODO
-# Given a dataframe, quasi-identifiers, and a sensitive columns
-# Return a dataframe with each equivilence class as a row and entropy
-# Of sensitive values for that class
+
 def get_l_entropy_table(dataset, q_identifiers, sensitive_column):
+    """
+    Given a dataframe, quasi-identifiers, and a sensitive columns
+    Return a dataframe with each equivilence class as a row and entropy
+    Of sensitive values for that class
+    """
     q_identifiers_sensitive = q_identifiers
     q_identifiers_sensitive.append(sensitive_column)
-    
-    # Get size of equivilence class
     equiv_class_sizes = dataset.groupby(q_identifiers[:-1]).size().to_frame().reset_index()
-    # Reset columns names
     equiv_class_sizes.columns = [*equiv_class_sizes.columns[:-1], 'equiv_class_size']
-
-    # Get count of sensitive values in each equivilence class
     sensitive_count = dataset.groupby(q_identifiers_sensitive).size().to_frame().reset_index()
-    # Reset columns names
     sensitive_count.columns = [*sensitive_count.columns[:-1], 'sensitive_count']
-    # Add equiv class counts back in
     sensitive_count_with_size = pd.merge(sensitive_count, equiv_class_sizes,left_on=q_identifiers[:-1], right_on=q_identifiers[:-1])
-    
     sensitive_count_with_size['prob'] = sensitive_count_with_size['sensitive_count'] / sensitive_count_with_size['equiv_class_size']
-    
     sensitive_count_with_size['log_prob'] = np.log(sensitive_count_with_size['prob'])
-    
     sensitive_count_with_size['neg_p_log_p'] = -1 * sensitive_count_with_size['prob'] * sensitive_count_with_size['log_prob']
-    
     sum_entropy = sensitive_count_with_size.groupby(q_identifiers[:-1]).agg({'neg_p_log_p':'sum'}).reset_index()
-    
     sum_entropy['total_entropy'] = np.exp(sum_entropy['neg_p_log_p'])
-
     return sum_entropy.drop('neg_p_log_p',axis=1)
 
-# Get entropy l-diversity for entire dataset
 def get_l_entropy(dataset, q_identifiers, sensitive_column):
+    """
+    Get entropy l-diversity for entire dataset
+    """
     entropy_table = get_l_entropy_table(dataset, q_identifiers, sensitive_column)
     return min(entropy_table['total_entropy'])
 

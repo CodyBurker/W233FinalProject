b51db80c6073e36cbf9c9604a8fe1af4a3c05d01
EC2 Default User <ec2-user@ip-172-31-58-69.ec2.internal>
2021-10-11 19:27:06 +0000

    Fixed test

--- tests/test_ldiversity.py
+++ tests/test_ldiversity.py
@@ -14,4 +14,4 @@ def test_get_l_distinct():
 def test_get_l_entropy():
     dataset = pd.read_csv("HW3.csv")
     output = ldiversity.get_l_entropy(dataset,['Sex'],'Drinks/Day')
-    assert output==4.2266662750043125
+    assert round(output,5)==round(4.2266662750043125,5)
